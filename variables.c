ae2fb42d2fd15bb0d4a8b62e4dcc46be07d76ea6
amolpandit38 <amol38.ap@gmail.com>
2022-08-30 21:39:27 +0530

    added variable file

--- variables.c
+++ variables.c
@@ -1,2 +1,22 @@
 // variable is the name of the memory location which is stores some data
- 
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   // variables are case sensitive
+   // variable can start with underscore or letter
+   // no comma/ blank space
+   // No other symbol other than '_'
+   // name a variable with meaning
+
+   int number = 25;
+   char star = '*';
+   int age = 22;
+   age = 24; // variable values can get updated
+   float pi = 3.14; 
+   int a = 30;
+   int A = 40;
+
+   return 0;
+}
